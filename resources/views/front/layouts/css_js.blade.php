4c5215b0347d09b804be481cc76998750403fa5d
unknown <payudarasemut900@gmail.com>
2020-06-01 02:44:40 +0700

    dikit

--- resources/views/front/layouts/css_js.blade.php
+++ resources/views/front/layouts/css_js.blade.php
@@ -1,4 +1,4 @@
-<!-- <script src="{{url('front_assets/cssAndJs/jquery-3.5.1.slim.min.js')}}"></script>
+<script src="{{url('front_assets/cssAndJs/jquery-3.5.1.slim.min.js')}}"></script>
 <link rel="stylesheet" href="{{url('front_assets/cssAndJs/bootstrap.min.css')}}">
 <script src="{{url('front_assets/cssAndJs/bootstrap.min.js')}}"></script>
 <script src="{{url('front_assets/cssAndJs/popper.min.js')}}"></script>
@@ -7,18 +7,8 @@
 <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@600&display=swap" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@800&display=swap" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@900&display=swap" rel="stylesheet">
-<script src="{{url('front_assets/cssAndJs/lottie.js')}}"></script> -->
+<script src="{{url('front_assets/cssAndJs/lottie.js')}}"></script>
 
-<script src="{{https('front_assets/cssAndJs/jquery-3.5.1.slim.min.js')}}"></script>
-<link rel="stylesheet" href="{{https('front_assets/cssAndJs/bootstrap.min.css')}}">
-<script src="{{https('front_assets/cssAndJs/bootstrap.min.js')}}"></script>
-<script src="{{https('front_assets/cssAndJs/popper.min.js')}}"></script>
-<script src="{{https('front_assets/cssAndJs/jquery.min.js')}}"></script>
-<script src="https://kit.fontawesome.com/835b438f3a.js" crossorigin="anonymous"></script>
-<link href="https://fonts.googleapis.com/css2?family=Nunito:wght@600&display=swap" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css2?family=Nunito:wght@800&display=swap" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css2?family=Nunito:wght@900&display=swap" rel="stylesheet">
-<script src="{{https('front_assets/cssAndJs/lottie.js')}}"></script>
 
 
 
