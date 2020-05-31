893bf9a275d7aa2b726d7427cdf5847d922cbfd7
unknown <payudarasemut900@gmail.com>
2020-06-01 02:41:51 +0700

    dikit

--- resources/views/front/layouts/css_js.blade.php
+++ resources/views/front/layouts/css_js.blade.php
@@ -9,16 +9,16 @@
 <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@900&display=swap" rel="stylesheet">
 <script src="{{url('front_assets/cssAndJs/lottie.js')}}"></script> -->
 
-<script src="https://front_assets/cssAndJs/jquery-3.5.1.slim.min.js"></script>
-<link rel="stylesheet" href="https://front_assets/cssAndJs/bootstrap.min.css">
-<script src="https://front_assets/cssAndJs/bootstrap.min.js"></script>
-<script src="https://front_assets/cssAndJs/popper.min.js"></script>
-<script src="https://front_assets/cssAndJs/jquery.min.js"></script>
+<script src="{{https('front_assets/cssAndJs/jquery-3.5.1.slim.min.js')}}"></script>
+<link rel="stylesheet" href="{{https('front_assets/cssAndJs/bootstrap.min.css')}}">
+<script src="{{https('front_assets/cssAndJs/bootstrap.min.js')}}"></script>
+<script src="{{https('front_assets/cssAndJs/popper.min.js')}}"></script>
+<script src="{{https('front_assets/cssAndJs/jquery.min.js')}}"></script>
 <script src="https://kit.fontawesome.com/835b438f3a.js" crossorigin="anonymous"></script>
 <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@600&display=swap" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@800&display=swap" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@900&display=swap" rel="stylesheet">
-<script src="https://front_assets/cssAndJs/lottie.js"></script>
+<script src="{{https('front_assets/cssAndJs/lottie.js')}}"></script>
 
 
 
