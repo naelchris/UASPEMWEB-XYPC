36b898c46d2c5ca93e600d5be840cba8e0d3d425
unknown <payudarasemut900@gmail.com>
2020-06-01 14:13:11 +0700

    dikit

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\product;
+use App\Product;
 use PHPUnit\Framework\Constraint\FileExists;
 
 class ProductController extends Controller
--- app/Http/Controllers/UserListController.php
+++ app/Http/Controllers/UserListController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\user;
+use App\User;
 use PHPUnit\Framework\Constraint\FileExists;
 
 class UserListController extends Controller
