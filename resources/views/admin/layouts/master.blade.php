9d7e313593ec8c0915aa46274160d7692195a0df
unknown <payudarasemut900@gmail.com>
2020-06-01 19:37:53 +0700

    asd

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -5,7 +5,7 @@
     <link rel="icon" type="image/png" sizes="96x96" href="https://glacial-falls-89341.herokuapp.com/assets/img/favicon.png">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
 
-    <title>TwitShop Admin</title>
+    <title>XYPC Admin</title>
 
     <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport'/>
     <meta name="viewport" content="width=device-width"/>
